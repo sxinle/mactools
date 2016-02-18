06584102671d028b1e8fcfc3eae41d8f3c3620b3
sxinle <9026687@qq.com>
2016-02-18 10:56:09 +0800

    aaa

--- swf.sh
+++ swf.sh
@@ -4,9 +4,6 @@ rootDir=/home/hsong/folder/util/mactools/swf
 
 filePath=`pwd`'/'$1
 
-echo $filePath
+sed -i 's#EMBED src=".*"#EMBED src="'$filePath'"#g' $rootDir/swf.html
 
-#sed -i 's/EMBED src=".*.swf"/EMBED src="'$filePath'"/g' $rootDir/swf.html
-
-#google-chrome $rootDir/swf.html
-
+google-chrome $rootDir/swf.html
