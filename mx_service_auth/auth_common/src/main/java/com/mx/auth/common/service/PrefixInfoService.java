9c63ee896f9dce184def888b5f3414888d07c05f
sxinle <9026687@qq.com>
2016-03-03 18:31:31 +0800

    update config

--- mx_service_auth/auth_common/src/main/java/com/mx/auth/common/service/PrefixInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.mx.auth.common.service;
-
-import com.mx.auth.common.model.PrefixInfo;
-
-public interface PrefixInfoService {
-
-	// 查询刮刮卡类型
-	Object getPrefixInfo(PrefixInfo bean) throws Exception;
-}
